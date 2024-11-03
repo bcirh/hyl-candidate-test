a33ef2800a70b01eab8bbe2722beb495d4690ab5
bcirh <72753984+bcirh@users.noreply.github.com>
2024-11-03 10:26:59 +0100

    Add consul, nomad, keycloack configuration (#1) (#2)
    * Remove nomad_server and nomad_agent logic.
    
    * Fix broken references in terraform code
    
    * Add nomad configuration and nomad psql and kc jobs
    
    * Add grafana nomad job. Add traefik let's encrypt cert resolver
    
    * Update variables.tf; Add ip restrictions default values
    
    * Add keycloak config task file
    
    * Add github actions starting template

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "monitoring_vm_public_ip" {
-  description = "Monitoring VM Public IP"
-  value       = azurerm_public_ip.monitoring_public_ip.ip_address
+output "hylastix_vm_public_ip" {
+  description = "VM Public IP"
+  value       = azurerm_public_ip.hylastix_minimal_public_ip.ip_address
 }
