b0762fbd6f8f9d33e329a9d3200d46c73aa57b8f
bcirh <72753984+bcirh@users.noreply.github.com>
2024-11-03 10:42:02 +0100

    Remove state file

--- terraform/state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    subscription_id      = var.subscription_id
-    resource_group_name  = "hylastix-tf-remote-backend"
-    storage_account_name = "hylastixftremotebackend"
-    container_name       = "hylastixftremotebackend" #one container per subscription 
-    key                  = "hylastix.tfstate"
-  }
-}
