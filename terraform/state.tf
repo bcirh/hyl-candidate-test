b365cc17fa838193a15fd98a05fc4b6d8a418624
bcirh <72753984+bcirh@users.noreply.github.com>
2024-11-03 12:03:30 +0100

    Github Actions manual trigger added

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    subscription_id      = var.subscription_id
+    subscription_id      = "32a2a8a0-04fb-4892-8b1a-30ac436c03de"
     resource_group_name  = "hylastix-tf-remote-backend"
     storage_account_name = "hylastixftremotebackend"
     container_name       = "hylastixftremotebackend" #one container per subscription 
