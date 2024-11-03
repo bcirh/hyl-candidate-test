95125a86c5710fe9b270f0f4b2e7e7229689cf1d
bcirh <72753984+bcirh@users.noreply.github.com>
2024-11-03 15:38:22 +0100

    fix port ranges

--- terraform/network.tf
+++ terraform/network.tf
@@ -51,8 +51,8 @@ resource "azurerm_network_security_group" "hylastix_minimal_nsg" {
       direction                  = security_rule.value["direction"]
       access                     = security_rule.value["access"]
       protocol                   = security_rule.value["protocol"]
-      source_port_range          = security_rule.value["source_port_range"]
-      destination_port_ranges    = security_rule.value["destination_port_ranges"]
+      source_port_ranges         = security_rule.value["source_port_ranges"]
+      destination_port_range     = security_rule.value["destination_port_range"]
       source_address_prefix      = security_rule.value["source_address_prefix"]
       destination_address_prefix = security_rule.value["destination_address_prefix"]
     }
